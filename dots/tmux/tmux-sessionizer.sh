fdaa5f854439ec0454681d2811353f898c999247
Rosen Popov <rdonkow@gmail.com>
2022-05-21 20:07:20 +0300

    Updated sessionizer

--- dots/tmux/tmux-sessionizer.sh
+++ dots/tmux/tmux-sessionizer.sh
@@ -7,7 +7,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/develop ~/.config ~/Downloads -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/develop ~/.config ~/Downloads ~/ScreepsArena -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
